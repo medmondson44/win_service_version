cc8cf07d021d977699673e0402860e8c6fd8c4f5
Marcus Edmondson <marcus.edmondson44@gmail.com>
2024-12-22 07:31:31 -0500

    Add files via upload

---
+++ win_service_version.ps1
@@ -0,0 +1,24 @@
+﻿# Get all services and their associated executable files, then get the version info
+Get-WmiObject -Class Win32_Service | ForEach-Object {
+    $service = $_
+    $exePath = $service.PathName
+    $exePath = $exePath -replace '"', ''  # Clean up any quotes around the path
+
+    # Exclude services whose file path contains "svchost"
+    if ($exePath -notlike "*svchost*") {
+        if (Test-Path $exePath) {
+            # Try to get the version of the executable
+            $fileVersion = (Get-Item $exePath).VersionInfo.FileVersion
+        } else {
+            $fileVersion = "N/A"
+        }
+
+        # Output the service name, display name, and version
+        [PSCustomObject]@{
+            ServiceName    = $service.Name
+            DisplayName    = $service.DisplayName
+            FilePath       = $exePath
+            Version        = $fileVersion
+        }
+    }
+} | fl *
